123691e3a0fdb97b4d027199a5f0929983ba21cd
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-01-04 17:19:16 +0800

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
-#Get the necessary components
-apt-get update
 apt-get install udisks2 -y
 echo " " > /var/lib/dpkg/info/udisks2.postinst
 apt-mark hold udisks2
 dpkg-reconfigure tzdata
-apt-get install xfce4 xfce4-goodies xfce4-terminal parole -y
-apt-get install tigervnc-standalone-server dbus-x11 -y
+apt-get install xfce4 xfce4-goodies xfce4-terminal parole tigervnc-standalone-server dbus-x11 -y
 apt-get --fix-broken install
-apt-get clean
 
 #Setup the necessary files
 mkdir -p ~/.vnc
-echo "#!/bin/bash
+echo "
+#!/bin/bash
 export PULSE_SERVER=127.0.0.1
 xrdb $HOME/.Xresources
-startxfce4" > ~/.vnc/xstartup
+startxfce4
+" > ~/.vnc/xstartup
 
-echo "#!/bin/sh
+echo "
+#!/bin/sh
 export DISPLAY=:1
 export PULSE_SERVER=127.0.0.1
 rm -rf /run/dbus/dbus.pid
-dbus-launch startxfce4" > /usr/local/bin/vncstart
+dbus-launch startxfce4
+" > /usr/local/bin/vncstart
    echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
    echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
    chmod +x ~/.vnc/xstartup
-   chmod +x /usr/local/bin/*
+   chmod 700 /usr/local/bin/*
+   
    clear
-   echo ""
-   echo "Installing Browser,.."
-   echo ""
 #Browser Fix
 wget https://raw.githubusercontent.com/wahasa/Ubuntu/main/Patch/passwd -P .vnc/
 apt install firefox-esr -y
