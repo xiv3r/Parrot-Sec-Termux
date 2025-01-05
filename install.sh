b3ee6a09ed1f8e8a1ab2e85ebf746a73d55cf5a6
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-01-05 09:35:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 pkg install root-repo x11-repo axel bsdtar proot xz-utils neofetch pulseaudio -y
 
-axel -a --sesrch -o parrot-arm64.tar.xz https://github.com/EXALAB/Anlinux-Resources/raw/refs/heads/master/Rootfs/Parrot/arm64/parrot-rootfs-arm64.tar.xz
+axel -a --search -o parrot-arm64.tar.xz https://github.com/EXALAB/Anlinux-Resources/raw/refs/heads/master/Rootfs/Parrot/arm64/parrot-rootfs-arm64.tar.xz
 
 echo "Decompressing Rootfs, please be patient."
 proot --link2symlink bsdtar -xpJf parrot-arm64.tar.xz
