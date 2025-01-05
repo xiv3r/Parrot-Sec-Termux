d8563fe8a7431ff6bf74561eaad9c30cc21a6e47
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-01-06 05:48:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,6 @@ pulseaudio --start \
     --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
     --exit-idle-time=-1
 EOF
-
 ### Ascii
 neofetch --ascii_distro Parrot -L
 
