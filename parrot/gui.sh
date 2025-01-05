36adc5f7d227bd407a443ca853fda6622f563df7
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-01-06 06:09:12 +0800

    Update gui.sh

--- parrot/gui.sh
+++ parrot/gui.sh
@@ -3,11 +3,11 @@
 apt install xfce4 xfce4-goodies xfce4-terminal parole tigervnc-standalone-server dbus-x11 tigervnc xorg-xhost -y
 
 ### Parrot vnc
-cat >/usr/bin/vncstart << EOF
+cat >/bin/vncstart << EOF
 #!/bin/bash
 export DISPLAY=:1
 export PULSE_SERVER=127.0.0.1
 startxfce4
 EOF
-chmod 700 /usr/bin/vncstart
+chmod 700 /bin/vncstart
 echo "vncstart >> .bashrc
