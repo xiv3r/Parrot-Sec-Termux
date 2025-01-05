76e28500c696e209fd728c161ad3b9d3e8e969fa
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-01-05 10:37:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+### install dependencies 
 pkg install root-repo x11-repo axel bsdtar proot xz-utils neofetch pulseaudio -y
-
+### Download Tarball
 axel -a --search -o parrot-arm64.tar.xz https://github.com/EXALAB/Anlinux-Resources/raw/refs/heads/master/Rootfs/Parrot/arm64/parrot-rootfs-arm64.tar.xz
-
-echo "Decompressing Rootfs, please be patient."
+### Decompress tarball
+echo "Decompressing Rootfs...!!!"
 proot --link2symlink bsdtar -xpJf parrot-arm64.tar.xz
-
+### pull config 
 wget -O .parrot https://raw.githubusercontent.com/xiv3r/Parrot-Sec-Termux/refs/heads/main/parrot/parrot
-echo "Fixing shebang"
+### Fix shebang
 termux-fix-shebang .parrot
 chmod +x .parrot
+### make executable
+echo "bash .parrot" >$PREFIX/bin/parrot
+chmod 700 $PREFIX/bin/parrot
